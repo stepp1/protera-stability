09f7f5f735273bb308e697d1bbaa65a349c8dfea
Stepp1 <sfaragg@gmail.com>
2021-08-02 01:27:16 -0400

    computed diversity, saved models, refactor, saved new datasets

--- parallel-synthesis/models.py
+++ parallel-synthesis/models.py
@@ -1,10 +1,52 @@
 from skorch.callbacks import Checkpoint, LRScheduler, EarlyStopping
 from skorch import NeuralNetRegressor
 
+import torch
 from torch import nn
 
-class ProteinMLP(nn.Module):
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+scoring = "r2"
+score = r2_score
+
+def perform_search(model, params, name, strategy = "grid", save_dir = "models"):
     
+    
+    if strategy == "grid":
+        searcher = GridSearchCV
+        
+    elif strategy == "bayes":
+        raise NotImplementedError
+        
+    
+    search = searcher(
+        estimator=model,
+        param_grid=params,
+        scoring=scoring,
+        verbose=1,
+        n_jobs=-1,
+        refit=True,
+    )
+    
+    print("============")
+    print(f'Fitting model {name}...')
+    grid.fit(X_train, y_train)
+    print(name, grid.best_score_)
+    print(grid.best_params_)
+    print("============")
+    
+    if "sklearn" in str(type(model)):
+        dump(grid, f"{save_dir}/best_{name}.joblib")
+        
+    else: 
+        # this assumes we're using skorch
+        torch.save(search.state_dict(), f"{save_dir}/best_{name}.pt")
+        
+    return search
+
+
+class ProteinMLP(nn.Module):    
     def __init__(self, n_in = 1280, n_units = 1024, act = None, drop_p = 0.7, last_drop = False):
         super(ProteinMLP, self).__init__()
         self.fc1 = nn.Linear(n_in, n_units)
