3ca991e3ab73448e922deadb923962486c6f30fc
stepp1 <sfaragg@gmail.com>
2021-08-24 17:00:46 -0400

    added PrintCallback

--- src/protera_stability/__init__.py
+++ src/protera_stability/__init__.py
@@ -2,19 +2,22 @@ import fsspec
 from .embeddings import EmbeddingExtractor1D
 from .models import ProteinMLP, perform_search
 from .train import (
+    AttrDict,
     LitProteins,
     ProteinStabilityDataset,
     SubsetDiversitySampler,
     LitProteins,
 )
-from .utils import open_train_test, dim_reduction
+from .utils import PrintCallback, open_train_test, dim_reduction
 
 __all__ = [
+    AttrDict,
     EmbeddingExtractor1D,
     ProteinMLP,
     LitProteins,
     ProteinStabilityDataset,
     SubsetDiversitySampler,
+    PrintCallback,
     open_train_test,
     perform_search,
     dim_reduction,
--- src/protera_stability/utils.py
+++ src/protera_stability/utils.py
@@ -1,12 +1,52 @@
+import copy
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import torch
+from IPython.display import clear_output, display
+from pytorch_lightning import Callback
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
 
 from embeddings import EmbeddingExtractor1D
 
+
+def unwrap(x):
+    if isinstance(x, torch.Tensor):
+        return x.item()
+    return x
+
+
+class PrintCallback(Callback):
+    def __init__(self):
+        self.metrics = []
+        self.cur_epoch = 0
+
+    def on_epoch_end(self, trainer, pl_module):
+        clear_output(wait=True)
+        metrics_dict = copy.deepcopy(trainer.callback_metrics)
+        
+        #del metrics_dict["loss"]
+        metrics_dict = {k: unwrap(v) for k, v in metrics_dict.items()}
+        metrics_dict["epoch"] = self.cur_epoch
+        self.cur_epoch += 1
+        self.metrics.append(metrics_dict)
+        
+        del metrics_dict
+        
+        
+        # column-names should be modified as per your usage
+        metrics_df = pd.DataFrame.from_records(
+            self.metrics,
+            columns=[
+                "epoch",
+                "train_loss",
+                "valid_loss",
+                "valid_r2",
+            ],
+        )
+        display(metrics_df)
 
 def dim_reduction(
     X,
