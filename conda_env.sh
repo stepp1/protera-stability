25e01d6a38dee1f52ca642b342b0b968771c9284
stepp1 <sfaragg@gmail.com>
2021-09-14 15:12:17 -0300

    adding fair-esm

--- conda_env.sh
+++ conda_env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# create conda env
 conda create -n protera-stability \
     pytorch torchvision torchaudio cudatoolkit=11.1 \
-    pytorch-lightning  pandas numpy matplotlib jupyterlab\
-    scikit-learn=0.23.2 scikit-optimize skorch \
-    biopython seaborn cloudpickle \
+    pytorch-lightning scikit-learn=0.23.2 scikit-optimize skorch \
+    pandas numpy matplotlib jupyterlab \
+    biopython seaborn cloudpickle dill \
     -c pytorch-nightly -c nvidia
+
+# install fair-esm
+$CONDA_PREFIX/envs/protera-stability/bin/python -m pip install fair-esm 
